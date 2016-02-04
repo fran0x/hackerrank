785e730e80af6ec471a5a101474da29f2526644c
teraflopx <teraflopx@gmail.com>
2016-02-04 07:47:01 +0100

    functional programming > adhoc; remove duplicates

---
+++ functional_programming/adhoc/RemoveDuplicates.scala
@@ -0,0 +1,9 @@
+//https://www.hackerrank.com/challenges/remove-duplicates
+
+object RemoveDuplicates extends App {
+
+  	val line = io.StdIn.readLine
+  	println(line.distinct)
+  	//line.toSet.mkString
+  	//scala.collection.mutable.SortedSet[Char]()
+}
