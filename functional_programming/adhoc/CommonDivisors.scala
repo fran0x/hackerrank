86d92db9d8220750481f3bba6333b0dcf8e3c585
teraflopx <teraflopx@gmail.com>
2016-02-05 16:54:28 +0100

    functional programming > adhoc; performance for common divisor is OK now

--- functional_programming/adhoc/CommonDivisors.scala
+++ functional_programming/adhoc/CommonDivisors.scala
@@ -4,7 +4,14 @@ import scala.language.postfixOps
 
 object CommonDivisors extends App {
 
-	def divisors(n: Int):List[Int] = (1 to n) filter (n % _ == 0) toList
+	// to measure time elapsed...
+	def time(f: => Unit) = {
+		val s = System.currentTimeMillis
+		f
+		System.currentTimeMillis - s
+	}
+
+	def divisors(n: Int):List[Int] = ((1 to n / 2) filter (n % _ == 0) toList) ::: List(n) // tried first (1 to n) but timeout
 	def commonDivisors(a: Int, b: Int):Int = divisors(a).intersect(divisors(b)).size
 	def parseLine(line: String):List[Int] = line.trim.split(" ").toList.map(_.toInt)
 
@@ -12,6 +19,7 @@ object CommonDivisors extends App {
 	val n = lines.next.toInt
 	for (i <- 1 to n) {
 		val line = parseLine(lines.next)
+		//println(time { commonDivisors(line(0), line(1)) } + "ms")
 		println(commonDivisors(line(0), line(1)))
 	}
 }
