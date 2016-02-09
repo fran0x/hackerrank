364c109bf8c40aeaa4b69fb5cf65f98dbff2abfc
teraflopx <teraflopx@gmail.com>
2016-02-09 22:45:40 +0100

    functional programming > recursion; super-queen, getting closer

--- functional_programming/recursion/SuperQueen.scala
+++ functional_programming/recursion/SuperQueen.scala
@@ -4,14 +4,23 @@
 object SuperQueen extends App {
 
 	type Queen = (Int, Int)
+	
+	// true if no other queen is attacking this position
 	def isSafe(queen: Queen, others: List[Queen]) = others forall (!isAttacked(queen, _))
-	def isAttacked(q: Queen, o: Queen) = {
-		q._1 == o._1 || // same row
-		q._2 == o._2 || // same column
-		(o._1 - q._1).abs == (o._2 - q._2).abs || // same diagonal
-		((o._1 - q._1).abs + (o._2 - q._2).abs) == 3 // knight
+	
+	// true if the other queen is attacking this position (moves: queen + knight)
+	def isAttacked(q: Queen, o: Queen) = q._1 == o._1 || q._2 == o._2 || (o._1 - q._1).abs == (o._2 - q._2).abs || ((o._1 - q._1).abs + (o._2 - q._2).abs) == 3
+    
+    def placeQueens(bsize: Int, col: Int): List[List[Queen]] = col match {
+		case 0 => List(Nil)
+		case _ => for {
+			queens <- placeQueens(col-1)
+			row <- 1 to col
+			queen = (col, row)
+			if (isSafe(queen, queens))
+		} yield queen :: queens
 	}
 
-	val n = io.StdIn.readInt
-	val board = Array.fill(n, n)(0)
+	val bsize = io.StdIn.readInt // board size
+    println(placeQueens(bsize, bsize).size)	
 }
