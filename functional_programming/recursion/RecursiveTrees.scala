a3d1ab9324447f6127249c66d0f4e36e919bb84e
teraflopx <teraflopx@gmail.com>
2016-02-14 14:41:56 +0100

    functional programming > recursion; recursive trees almost done

--- functional_programming/recursion/RecursiveTrees.scala
+++ functional_programming/recursion/RecursiveTrees.scala
@@ -2,5 +2,31 @@
 
 object RecursiveTrees extends App {
 
-	val n = io.StdIn.readInta
+	val Rows = 63
+	val Columns = 100
+	val Empty = List.empty
+	def full = List.fill(Columns)('_')
+
+	class Row(vert: List[Int], left: List[Int], right: List[Int]) {
+		def fork = { val tuple = for (v <- vert) yield (v - 1, v + 1); Row(Empty, tuple map (_._1), tuple map (_._2)) }
+		def vertical = Row(left ::: right, Empty, Empty)
+		def next = vert match {
+			case Empty => Row(Empty, left map (_ - 1), right map (_ + 1))
+			case _ => Row(vert, Empty, Empty)
+		}
+		//def toString = { val string = Full; (vert ::: left ::: right) map (string(_) = '1') }
+	}
+	object Row { def apply(vert: List[Int], left: List[Int], right: List[Int]) = new Row(vert, left, right) }
+
+	/*
+	def f(n: Int, prev: Row):List[List[Int]] = n match {
+		case 0 => List.empty
+		case _ =>  
+		}
+	}
+	*/
+
+	val n = io.StdIn.readInt
+	//val result = f(n, Empty)
+	//(1 until n) map (i => println(result(i).mkString))
 }
