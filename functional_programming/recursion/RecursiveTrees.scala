4bf337b552f710f19c0bcaa45702454b841f4e80
teraflopx <teraflopx@gmail.com>
2016-02-15 07:15:38 +0100

    functional programming > recursion; recursive trees, completed! needs refinement though...

--- functional_programming/recursion/RecursiveTrees.scala
+++ functional_programming/recursion/RecursiveTrees.scala
@@ -1,12 +1,19 @@
 //https://www.hackerrank.com/challenges/fractal-trees
 
+
+
 object RecursiveTrees extends App {
 
+	import scala.collection.mutable.ListBuffer
+	
 	val Rows = 63
 	val Columns = 100
+	val Center = 49
+	val Segment = 16
 	val Empty = List.empty
 
 	def full = Array.fill(Columns)('_')
+	val fullOut = full.mkString
 
 	class Row(val vert: List[Int], val left: List[Int], val right: List[Int]) {
 		def fork = { val tuple = for (v <- vert) yield (v - 1, v + 1); Row(Empty, tuple map (_._1), tuple map (_._2)) }
@@ -23,21 +30,23 @@ object RecursiveTrees extends App {
 		case 0 => List.empty
 		case _ => segpos match {
 			case 1 => {
-					if (prev.vert == Empty) {
+					if (prev.vert == Empty) { // next iteration!
 						val next = prev.vertical
-						next.out :: f(n - 1, seg / 2, seg / 2, next)						
+						if (n > 1) next.out :: f(n - 1, seg / 2, seg / 2, next)
+						else f(n - 1, seg / 2, seg / 2, next)				
 					}
 					else {
 						val next = prev.fork
-						next.out :: f(n - 1, seg, seg, next)
+						next.out :: f(n, seg, seg, next)
 					}
 			}
-			case _ => val next = prev.next; next.out :: f(n - 1, seg, segpos - 1, next)
+			case _ => val next = prev.next; next.out :: f(n, seg, segpos - 1, next)
 		}  
 	}
 
-	val n = io.StdIn.readInt
-	val row = Row(List(49), Empty, Empty) 
-	val out = f(n, 16, 16, row)	
+	val n = io.StdIn.readInt // number of iterations (n <= 5)
+	val row = Row(List(Center), Empty, Empty) 
+	val out = (row.out :: f(n, Segment, Segment, row)).to[ListBuffer]		
+	for (i <- 1 to (Rows - out.size)) out += fullOut
 	out.reverse map (println)
 }
