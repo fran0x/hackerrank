c34a571f69f875376c9963fcc00276e3a5ba30bb
teraflopx <teraflopx@gmail.com>
2016-02-16 22:12:30 +0100

    functional programming > recursion; sierpinski triangles, started problem analysis

--- functional_programming/recursion/SierpinskiTriangles.scala
+++ functional_programming/recursion/SierpinskiTriangles.scala
@@ -6,5 +6,15 @@ object SierpinskiTriangles extends App {
 	val Columns = 63
 	val Center = 31
 
+	// iteration 0: triangle = 32 rows x 63 columns
+	// iteration 1: triangle = 16 rows x 31 columns >> 16 = 32 / 2; 31 = (63 - 1) / 2
+	// iteration 2: triangle = 8 rows x 15 columns
+	// iteration 3: triangle = 4 rows x 7 columns
+	// iteration 4: triangle = 2 rows x 3 columns
+	// iteration 5: triangle = 1 row x 1 column
+
+	// first generate triangle iteration 0
+	// at each iteration an input is the triangle from the previous iteration, and then, based on the indexes, we change elements from 1 to _
+	
 	val n = io.StdIn.readInt // number of iterations (n <= 5)
 }
