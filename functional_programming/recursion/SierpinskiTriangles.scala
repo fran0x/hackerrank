fecd59f59aaae938728b2ac0675a330f0b5f6a3a
teraflopx <teraflopx@gmail.com>
2016-02-17 08:02:20 +0100

    functional programming > recursion; added triangle for iteration 0, pending reverse

--- functional_programming/recursion/SierpinskiTriangles.scala
+++ functional_programming/recursion/SierpinskiTriangles.scala
@@ -6,6 +6,8 @@ object SierpinskiTriangles extends App {
 	val Columns = 63
 	val Center = 31
 
+	def ones(x: Int) = Array.fill(x)('_') ++ Array.fill(Columns - 2 * x)('1') ++ Array.fill(x)('_')
+	
 	// iteration 0: triangle = 32 rows x 63 columns
 	// iteration 1: triangle = 16 rows x 31 columns >> 16 = 32 / 2; 31 = (63 - 1) / 2
 	// iteration 2: triangle = 8 rows x 15 columns
@@ -13,8 +15,11 @@ object SierpinskiTriangles extends App {
 	// iteration 4: triangle = 2 rows x 3 columns
 	// iteration 5: triangle = 1 row x 1 column
 
-	// first generate triangle iteration 0
 	// at each iteration an input is the triangle from the previous iteration, and then, based on the indexes, we change elements from 1 to _
-	
+
 	val n = io.StdIn.readInt // number of iterations (n <= 5)
+
+	// generate the triangle for iteration 0
+	val triangle = Array.ofDim[Char](Rows, Columns)
+	(0 until Rows) foreach (x => triangle(x) = ones(x))	
 }
