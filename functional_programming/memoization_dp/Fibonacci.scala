a9e0dbef6566658ab815df7279b8fb08321f2966
teraflopx <teraflopx@gmail.com>
2016-02-20 11:32:59 +0100

    functional programming > memoization and dynamic programming; fibonacci 0(n)

--- functional_programming/memoization_dp/Fibonacci.scala
+++ functional_programming/memoization_dp/Fibonacci.scala
@@ -1,35 +1,35 @@
 //https://www.hackerrank.com/challenges/fibonacci-fp
 
 // applying dynamic programming; performance goes from O(2^n) to O(n)
+// my first approach was based on BigInt but 4th test case timed out: I did not pay attention to the point about module (see link below)
 object Fibonacci extends App {
 
   import scala.annotation.tailrec
   
-  val Zero = BigInt(0)
-  val One = BigInt(1)
-  val Mod = BigInt(10).pow(8) + 7
-
-  def print(fib: BigInt) = println(fib % Mod)
+  val Zero = 0
+  val One = 1
+  val Mod = (math.pow(10, 8) + 7).toInt
 
   // fib(0) = 0
   // fib(1) = 1
   // fib(n) = fib(n - 1) + fib(n - 2) // for n > 1
   // https://en.wikipedia.org/wiki/tail_call
+  // https://codeaccepted.wordpress.com/2014/02/15/output-the-answer-modulo-109-7/
   @tailrec
-  def fibonacciTail(x: BigInt, a: BigInt, b: BigInt): BigInt = {
+  def fibonacciTail(x: Int, a: Int, b: Int): Int = {
     x match {
-   		case z: BigInt if z == 0 => b // BigInt is not a case class!
-    	case _ => fibonacciTail(x - 1, b, a + b)
+   		case Zero => b
+    	case _ => fibonacciTail(x - 1, b, (a + b) % Mod)
   	}
   }
 
-  def fibonacci(x: BigInt):BigInt = {
+  def fibonacci(x: Int):Int = {
   	x match {
-  		case z: BigInt if z == 0 => Zero
+  		case Zero => Zero
   		case _ => return fibonacciTail(x, One, Zero)
   	}		
   }
 
 	val n = io.StdIn.readInt
-  for (i <- 1 to n) print(fibonacci(io.StdIn.readInt))
+  for (i <- 1 to n) println(fibonacci(io.StdIn.readInt))
 }
