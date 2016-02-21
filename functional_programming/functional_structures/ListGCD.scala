d6eb34d6a5e5954afb669b7a801b0cc3432c9fec
teraflopx <teraflopx@gmail.com>
2016-02-21 19:23:41 +0100

    functional programming > functional structures; lists and gcd, better with a type

--- functional_programming/functional_structures/ListGCD.scala
+++ functional_programming/functional_structures/ListGCD.scala
@@ -4,15 +4,16 @@ object ListGCD extends App {
 
   import scala.collection.mutable.{HashMap => MMap}
 
+  type Factors = List[(Int, Int)]
   val EmptyList = List[Int]()
   val mem = MMap[Int, List[Int]]()
 
-  def parseLine(line: String):List[(Int, Int)] = line.split(" ").map(_.toInt).grouped(2).toList.map(x => (x(0), x(1)))  
-  def add(xs: List[(Int, Int)]) = for ((prime, exp) <- xs) mem += (prime -> (exp :: mem.getOrElse(prime, EmptyList)))
+  def parseLine(line: String):Factors = line.split(" ").map(_.toInt).grouped(2).toList.map(x => (x(0), x(1)))  
+  def addFactors(factors: Factors) = for ((prime, exp) <- factors) mem += (prime -> (exp :: mem.getOrElse(prime, EmptyList)))
   def gcd(n: Int) = for ((prime, exps) <- mem if exps.size == n) yield (prime, exps.min)
 
   val n = io.StdIn.readInt
-  for (i <- 1 to n) add(parseLine(io.StdIn.readLine))
+  for (i <- 1 to n) addFactors(parseLine(io.StdIn.readLine))
 
   val output = gcd(n).toList.sorted.flatMap(x => x.productIterator.toList)
   println(output.mkString(" "))
