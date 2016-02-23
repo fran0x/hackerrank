e6283b2f60be6df3c7a2419c4383f09b6db2a324
teraflopx <teraflopx@gmail.com>
2016-02-23 21:05:24 +0100

    functional programming > functional structures; valid BST completed, easy one

--- functional_programming/functional_structures/ValidBST.scala
+++ functional_programming/functional_structures/ValidBST.scala
@@ -2,4 +2,19 @@
 
 object ValidBST extends App {
 
+	def isBST(L: List[Int]):Boolean = L match {
+		case Nil => true
+		case x :: xs => {
+			val (left, right) = xs.span(_ < x)
+			if (right.filter(_ < x).size > 0) false
+			else isBST(left) && isBST(right)
+		}
+	}
+
+	val T = io.StdIn.readInt // number of test cases
+	for (t <- 1 to T) {
+		val N = io.StdIn.readInt // number of nodes
+		val L = io.StdIn.readLine.split(" ").map(_.toInt).toList // preorder
+		println(if (isBST(L)) "YES" else "NO")
+	}
 }
