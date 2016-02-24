cac2ce01b8fd092b9bf4de40c29da59c2c8361df
teraflopx <teraflopx@gmail.com>
2016-02-24 21:52:07 +0100

    functional programming > functional structures; substring searching now performs well with large inputs

--- functional_programming/functional_structures/SubstringSearching.scala
+++ functional_programming/functional_structures/SubstringSearching.scala
@@ -5,14 +5,41 @@ object SubstringSearching extends App {
 
 	import scala.annotation.tailrec
 
+	def makeTable(pat: Array[Char]) = {
+		val table = Array.fill(pat.size)(0)
+		table(0) = -1
+		table(1) = 0
+
+		var pos = 2
+		var cnd = 0
+		while (pos < pat.size) {
+			if (pat(pos - 1) == pat(cnd)) {
+				table(pos) = cnd + 1
+				cnd += 1
+				pos += 1
+			}
+			else if (cnd > 0) cnd = table(cnd)
+			else {
+				table(pos) = 0
+				pos += 1
+			}
+		}
+
+		table
+	}
+
 	def isPresent(pat: Array[Char], text: Array[Char]) = {
+		val T = if (pat.size == 1) Array(-1) else makeTable(pat)
+		//println(T.mkString(" "))
+
 		@tailrec
 		def loop(p: Int, t: Int):Boolean = { // the scala idiomatic way... (aka "no returns please")
 			if (p + t > text.size - 1) false
 			else if (pat(p) == text(p + t)) {
 				if (p == pat.size - 1) true else loop(p + 1, t)
 			}
-			else loop(0, t + 1)
+			else if (T(p) > -1) loop(T(p), p + t - T(p)) // backtracking
+            else loop(0, t + 1)
 		}
 
 		if (pat.size > text.size) false // easy check
