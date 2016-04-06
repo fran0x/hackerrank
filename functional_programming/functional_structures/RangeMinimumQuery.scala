da6cf3a9f7d3bff2c193331d4a3ac98f9475adb0
teraflopx <teraflopx@gmail.com>
2016-04-06 07:12:50 +0200

    functional programming > functional structures; range minimum query, segment tree built with success

--- functional_programming/functional_structures/RangeMinimumQuery.scala
+++ functional_programming/functional_structures/RangeMinimumQuery.scala
@@ -9,9 +9,11 @@ object RangeMinimumQuery extends App {
 		def min(range: Range): Int
 	}
 	case class Leaf(idx: Int, min: Int) extends SegmentTree {
+		println(idx + "-" + idx + ":" + min)
 		override def min(range: Range): Int = min
 	}
 	case class Node(range: Range, min: Int, leftTree: SegmentTree, rightTree: SegmentTree) extends SegmentTree {
+		println(range.left + "-" + range.right + ":" + min)		
 		override def min(range: Range): Int = min
 	}
 
@@ -22,7 +24,7 @@ object RangeMinimumQuery extends App {
 				else {
 					val center = (range.left + range.right) / 2
 					val leftTree = buildTree(Range(range.left, center))
-					val rightTree = buildTree(Range(center, range.right))
+					val rightTree = buildTree(Range(center + 1, range.right))
 					Node(range, math.min(leftTree.min, rightTree.min), leftTree, rightTree)
 				}
 			}
@@ -35,5 +37,6 @@ object RangeMinimumQuery extends App {
 	val A = lines.take(1).toList(0).split(" ").map(_.toInt)
 	val Q = lines.take(n).toList.map(_.split(" ").map(_.toInt))
 
-	Q map { case Array(left, right) => println(A.slice(left, right).min) }
+	//Q map { case Array(left, right) => println(A.slice(left, right).min) }
+	SegmentTree.build(A)
 }
