8e3130667dc02cc3c8416d78014d9cbf2ebd8b69
teraflopx <teraflopx@gmail.com>
2016-04-08 18:50:22 +0200

    functional programming > functional structures; stocks prediction works for all inputs thanks to faster read, cache prev results and using indexes

--- functional_programming/functional_structures/StocksPrediction.scala
+++ functional_programming/functional_structures/StocksPrediction.scala
@@ -2,20 +2,36 @@
 
 object StocksPrediction extends App {
 
-	def search(A: List[Int], d: Int, m: Int) = {
-		val (left, target :: right) = A splitAt(d)
-		def inRange(a: Int) = (a >= target && a <= target + m)		
+	case class Query(d: Int, m: Int)
 
-		val (leftIn, leftOut) = left.reverse span (inRange(_))
-		val (rightIn, rightOut) = right span (inRange(_))
-		leftIn.size + 1 + rightIn.size
+	import scala.collection.mutable.{HashMap ⇒ MMap}
+	val cache = MMap[Query, Int]() // let's cache prev results
+
+	// reading inputs like this is faster than following io.Source.stdin.getLines approach
+    val N = io.StdIn.readInt()
+    val A = io.StdIn.readLine().split(' ').toArray.map(_.toInt)
+    val Q = io.StdIn.readInt()
+
+	def search(d: Int, m: Int) = {
+		val low = A(d)
+		val high = low + m
+		def offRange(a: Int) = (a < low || a > high)		
+
+		// let's play with indexes
+		def searchLeft(idx: Int): Int = {
+			if (idx < 0 || offRange(A(idx))) d - (idx + 1)
+			else searchLeft(idx - 1)
+		}
+		def searchRight(idx: Int): Int = {
+			if (idx >= N || offRange(A(idx))) idx - d
+			else searchRight(idx + 1)			
+		}
+
+		searchLeft(d - 1) + searchRight(d + 1)
     }
 
-	val lines = io.Source.stdin.getLines
-	val a = lines.take(1).toList(0).toInt
-	val A = lines.take(1).toList(0).split(" ").map(_.toInt).toList // require(A.length == a)
-	val q = lines.take(1).toList(0).toInt
-	val Q = lines.take(q).toList.map(_.split(" ").map(_.toInt))
-
-	Q map { case Array(d, m) => println(search(A, d, m)) }
+	for (_ <- 1 to Q) {
+       val Array(d, m) = io.StdIn.readLine().split(' ').toArray.map(_.toInt)
+       println(cache getOrElseUpdate (Query(d, m), search(d, m)))
+    }
 }
