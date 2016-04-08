8ae30efdf7a2d3046110a089c57969bcd96ef109
teraflopx <teraflopx@gmail.com>
2016-04-08 07:31:05 +0200

    functional programming > functional structures; stocks prediction works for small inputs, second try

--- functional_programming/functional_structures/StocksPrediction.scala
+++ functional_programming/functional_structures/StocksPrediction.scala
@@ -2,27 +2,18 @@
 
 object StocksPrediction extends App {
 
-	def search(A: Array[Int], d: Int, m: Int) = {
-		def inRange(a: Int) = (a >= A(d) && a <= A(d) + m)
-    	var max, c = 0
+	def search(A: List[Int], d: Int, m: Int) = {
+		val (left, target :: right) = A splitAt(d)
+		def inRange(a: Int) = (a >= target && a <= target + m)		
 
-		for (a <- A) {
-      		if (inRange(a)) {
-      			c += 1
-      			if (c > max) max = c
-      		}
-      		else {
-      			if (c > max) max = c
-      			c = 0
-      		}
-      		//println(a + ":" + c + ":" + max)
-      	}
-      	max
+		val (leftIn, leftOut) = left.reverse span (inRange(_))
+		val (rightIn, rightOut) = right span (inRange(_))
+		leftIn.size + 1 + rightIn.size
     }
 
 	val lines = io.Source.stdin.getLines
 	val a = lines.take(1).toList(0).toInt
-	val A = lines.take(1).toList(0).split(" ").map(_.toInt) // require(A.length == a)
+	val A = lines.take(1).toList(0).split(" ").map(_.toInt).toList // require(A.length == a)
 	val q = lines.take(1).toList(0).toInt
 	val Q = lines.take(q).toList.map(_.split(" ").map(_.toInt))
 
