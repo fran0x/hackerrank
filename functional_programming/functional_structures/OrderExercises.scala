6d73eb28a8010278310c3d98a9e4bbd47b69dec6
teraflopx <teraflopx@gmail.com>
2016-04-30 22:11:10 +0200

    functional programming > functional structures; order exercises (max subarray problem), kadane's algorithm

--- functional_programming/functional_structures/OrderExercises.scala
+++ functional_programming/functional_structures/OrderExercises.scala
@@ -3,7 +3,18 @@
 // see https://en.wikipedia.org/wiki/Maximum_subarray_problem
 object OrderExercises extends App {
 
+	def max(A: Array[Int]): Int = {
+		var maxLocal, maxTotal = 0
+		for (a <- A) {
+			maxLocal = math.max(0, maxLocal + a)
+			maxTotal = math.max(maxTotal, maxLocal)
+		}
+		maxTotal
+	}
+	
 	val lines = io.Source.stdin.getLines	
 	val Array(n, k) = lines.take(1).toList(0).split(" ").map(_.toInt)
-	val arr = lines.take(1).toList(0).split(" ").map(_.toInt)
+	val A = lines.take(1).toList(0).split(" ").map(_.toInt)
+	
+	println(max(A))
 }
