0a95ecf671203a2fd815530fbae8e21f85503407
Francisco Lopez <flopezlasanta@gmail.com>
2016-05-02 17:00:22 +0200

    added proposal to solve the problem

--- functional_programming/functional_structures/OrderExercises.scala
+++ functional_programming/functional_structures/OrderExercises.scala
@@ -3,6 +3,7 @@
 // see https://en.wikipedia.org/wiki/Maximum_subarray_problem
 object OrderExercises extends App {
 
+	// kadane
 	def max(A: Array[Int]): Int = {
 		var maxLocal, maxTotal = 0
 		for (a <- A) {
@@ -11,6 +12,11 @@ object OrderExercises extends App {
 		}
 		maxTotal
 	}
+	// another approach, more FP: A.scanLeft(0)((acc, n) => math.max(0, acc + n)).max
+	
+	// ...but that´s not enough to solve this problem thus try the following:
+	// run kadane and return a triplet (max, start_idx, end_idx)
+	// recursively run kadane in both sides (0 to start_idx, end_idx to end), add more maxs
 	
 	val lines = io.Source.stdin.getLines	
 	val Array(n, k) = lines.take(1).toList(0).split(" ").map(_.toInt)
