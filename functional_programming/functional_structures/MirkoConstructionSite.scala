f51861d5ceea4d86425517034d91396f75a1e8ba
Francisco Lopez <flopezlasanta@gmail.com>
2016-05-21 12:15:52 +0200

    functional programming > functional structures; drafted different approach for Mirko construction

--- functional_programming/functional_structures/MirkoConstructionSite.scala
+++ functional_programming/functional_structures/MirkoConstructionSite.scala
@@ -15,4 +15,12 @@ object MirkoConstructionSite extends App {
 	def find(q: Int) = build(q).zipWithIndex.maxBy(x => (x._1, x._2))._2 + 1 // see http://reactive.xploregroup.be/blog/8/Sorting-on-multiple-fields-and-Tuples
 
 	for (_ <- 1 to q) println(find(io.StdIn.readInt()))
+	
+	// try the following:
+	// 1. read all steps and get the max
+	// 2. start by the min step and calculate order
+	// 3. continue with next step (reuse prev calculation?) and compare order:
+	//    a. no changes then same result
+	//    b. changes then last building is candidate to be ignored for further calculations
+	//    c. by removing buildings we will eventually reach a point in which only 1 building will remain
 }
