2027752418a3ef69486eda45110cc5ff0400827e
Francisco Lopez <flopezlasanta@gmail.com>
2016-05-22 09:48:25 +0200

    functional programming > functional structures; drafted different approach for Mirko construction, extended

--- functional_programming/functional_structures/MirkoConstructionSite.scala
+++ functional_programming/functional_structures/MirkoConstructionSite.scala
@@ -17,10 +17,10 @@ object MirkoConstructionSite extends App {
 	for (_ <- 1 to q) println(find(io.StdIn.readInt()))
 	
 	// try the following:
-	// 1. read all steps and get the max
-	// 2. start by the min step and calculate order
-	// 3. continue with next step (reuse prev calculation?) and compare order:
-	//    a. no changes then same result
-	//    b. changes then last building is candidate to be ignored for further calculations
-	//    c. by removing buildings we will eventually reach a point in which only 1 building will remain
+	// 1. read all steps and get ordered list with the indexes mapped (step 1 -> query 3, step 2 -> query 7, step 5 -> query 1)
+	// 2. start by the min step and calculate order (step 1 -> 1, 4, 5, 2, 7, 3, 6); query 3 (step 1) = 1
+	// 3. continue with next step (reuse prev calculation?) and compare order: (step 2 -> 1, 4, 2, 5, 7, 6, 3)
+	//    a. no changes then same result: query 7 (step 2) = 1
+	//    b. changes then some buildings are candidate to be ignored for further calculations: 5 and 3 go down then ignored from now on
+	// by removing buildings we will eventually reach a point in which only 1 building will remain
 }
