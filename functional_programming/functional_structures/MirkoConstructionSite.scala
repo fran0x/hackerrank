f29d2f77b437f2e13ba5126a46a83154adbb4c55
Francisco Lopez-Lasanta (elopefr) <francisco.lopez-lasanta@ericsson.com>
2016-05-29 15:31:26 +0200

    functional programming > functional structures; mirko with a different approach (not working yet)

--- functional_programming/functional_structures/MirkoConstructionSite.scala
+++ functional_programming/functional_structures/MirkoConstructionSite.scala
@@ -1,34 +1,94 @@
 //https://www.hackerrank.com/challenges/mirko-at-construction-site
 
+object Control {
+ 
+ 	import scala.language.reflectiveCalls
+
+	def using[A <: { def close(): Unit }, B](param: A)(f: A => B): B =
+		try {
+			f(param)
+		} finally {
+			param.close()
+		}
+
+}
+
+object Perf {
+	import java.util.concurrent.TimeUnit
+	def time[R](block: => R): R = {  
+    	val s = System.nanoTime()
+    	val r = block    // call-by-name
+    	println("Elapsed time: " + TimeUnit.SECONDS.convert(System.nanoTime() - s, TimeUnit.NANOSECONDS) + " seconds")
+    	r
+	}
+}
+
 object MirkoConstructionSite extends App {
 
-	val Array(n, q) = io.StdIn.readLine().split(' ').toArray.map(_.toInt) // n = number of buildings, q = number of queries
-	val base = io.StdIn.readLine().split(' ').toList.map(_.toInt) // base height
-	val step = io.StdIn.readLine().split(' ').toList.map(_.toInt) // step height
+    import Control._
+    def readLines(filename:String):Option[Iterator[String]] = {
+    	try {
+    		val lines = using(io.Source.fromFile(filename)) {
+    			source => (for (line <- source.getLines) yield line).toList.iterator
+    		}
+    		Some(lines)
+    	} catch {
+    		case e: Exception => None
+    	}
+    }
 
-	// (bs: building indexes, q: steps) = Map(height -> building index)
-	def build(bs:List[Int], q: Int) = bs.map(b => (base(b) + step(b) * q, b)).sorted.toMap
+    def run(lines: Iterator[String]):Unit = {
+		val Array(n, q) = lines.next.split(' ').toArray.map(_.toInt) // n = number of buildings, q = number of queries
+		//println(s"n:${n} q:${q}")
+		val base = lines.next.split(' ').toList.map(_.toInt) // base height
+		val step = lines.next.split(' ').toList.map(_.toInt) // step height
+		//println(s"bases:${base.length} steps:${step.length}")
 
-	// buildings (indexes) in the game
-	var buildings = (0 until n).toList
+		// (bs: building indexes, q: steps) = List((height,building index)) ordered from tallest to shortest
+		def build(bs:List[Int], q: Int) = 
+			bs.
+			map(b => (base(b) + step(b) * q, b)).
+			sorted(Ordering[(Int,Int)].reverse).
+			unzip._2.
+			zipWithIndex
 
-	// create a list of tuples (query, position)	
-	var queries = List[Int]()
-	for (_ <- 1 to q) queries ++= List(io.StdIn.readInt())
-	val sortedQueries = queries.zipWithIndex.sorted // List[(query, original position)]
+		// buildings (indexes) in the game
+		var buildings = (0 until n).toList
 
-	var answers = Map[Int, Int]() // Map(query -> building index)
+		// create a list of tuples (query, position)	
+		val queries = new Array[Int](q)
+		(0 until q).foreach(queries(_) = lines.next.toInt)
+	 	val sortedQueries = queries.zipWithIndex.sorted // List[(query, original position)]
+		//println(s"queries:${queries.length}")
 
-	// this is to get rid of buildings that get a worse position from iteration to iteration	
-	var prev = Map[Int, Int]()
-	def down(x:(Int, Int)) = if (prev(x._1) > x._2) true else false
+	 	import scala.collection.mutable.{Map => MutableMap}
+		var answers = MutableMap[Int, Int]() // Map(query -> building index)
 
-	for (query <- sortedQueries) {
-		val actual = build(buildings, query._1)
-		answers ++= Map(query._1 -> actual.head._1)
+		// this is to get rid of buildings that get a worse position from iteration to iteration	
+		var prev = Map[Int, Int]()
 
-		if (!prev.isEmpty) buildings = actual.filterNot(down).unzip._1.toList
-		prev = actual
+		import Perf._
+		time {
+		    for (query <- sortedQueries) {
+		    	print(buildings.length + " ")
+		        if (buildings.length == 1) answers += (query._1 -> (buildings(0) + 1))
+		        else {
+					val actual = build(buildings, query._1) // building -> position
+					answers += query._1 -> (actual.head._1 + 1)
+
+			        if (!prev.isEmpty) buildings = for (x <- actual if (prev(x._1) >= x._2)) yield x._1
+					prev = actual.toMap
+		        }
+			}
+		}
+		//for (query <- queries) println(answers(query)) 
 	}
-	for (query <- queries) println(answers(query))  
+
+	if (args.length == 1) {
+		readLines(args(0)) match {
+			case Some(lines) => run(lines)
+			case None => println("No lines")
+		}
+	}
+	else println("No input")
 }
